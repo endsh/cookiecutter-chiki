e1f3e8011f16dbc5ca6de2e9238e6bbe05813e58
linshao <438985635@qq.com>
2016-01-06 14:27:29 +0800

    支持通用模型

--- {{cookiecutter.name}}/{{cookiecutter.name}}/admin/__init__.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}/admin/__init__.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from chiki import init_uploads
 from chiki.admin import Admin, ModelView, IndexView, get_static_admin
+from chiki.contrib.common import Item, StatLog, TraceLog
+from chiki.contrib.common import ItemView, StatLogView, TraceLogView
 from {{ cookiecutter.name }}.base import db
 from {{ cookiecutter.name }}.config import BaseConfig, WebConfig
 
@@ -20,6 +22,11 @@ def init(app):
         u'运营': 'fa fa-hdd-o',
         u'日志': 'fa fa-database',
     }
+
+    # 日志
+    admin.add_view(ItemView(Item,           name='系统选项', category='日志'))
+    admin.add_view(StatLogView(StatLog,     name='统计日志', category='日志'))
+    admin.add_view(TraceLogView(TraceLog,   name='跟踪日志', category='日志'))
 
     WebStaticAdmin = get_static_admin('WebStaticAdmin')
     admin.add_view(WebStaticAdmin(WebConfig.RELEASE_STATIC_FOLDER,
