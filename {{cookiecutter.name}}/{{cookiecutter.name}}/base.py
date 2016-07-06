1539f6b0daedc58db8261fe7bbb1e99c02bee5fa
tinysh <438985635@qq.com>
2016-07-06 11:01:50 +0800

    更新

--- {{cookiecutter.name}}/{{cookiecutter.name}}/base.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}/base.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 from chiki.base import db
+from chiki.cool import cm
 from chiki.contrib.users import UserManager
 from chiki.api import api, wapi
 from chiki.oauth import WeRoBot
-{% if cookiecutter.has_api -%}
+{%- if cookiecutter.has_api %}
 from flask import Blueprint
 {%- endif %}
-
 from {{ cookiecutter.name }}.config import BaseConfig
 
 um = UserManager()
